6a808a07c4d6cf56bc69b0552214483b21035ce8
ceeja <conradj26@gmail.com>
2024-05-15 18:31:27 -0400

    console log

--- INST377 Group Project Homepage Javascript.js
+++ INST377 Group Project Homepage Javascript.js
@@ -1,4 +1,7 @@
 function FetchAPI(){ 
     return fetch(`https://api-fulfill.dataexchange.us-east-1.amazonaws.com/v1`)
-    .then((res) => res.json());
+    .then((res) => res.json())
+    .then((data) =>{
+        console.log(data)
+    })
 }
