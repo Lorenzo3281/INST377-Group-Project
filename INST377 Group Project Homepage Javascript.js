2e0872c248cc50ac80f2cb4972e1d4ec9661006b
Lorenzo3281 <lorenzoa@terpmail.umd.edu>
2024-05-15 19:24:23 -0400

    Fixed token authentication and got new API

--- INST377 Group Project Homepage Javascript.js
+++ INST377 Group Project Homepage Javascript.js
@@ -1,5 +1,9 @@
-function FetchAPI(){ 
-    return fetch(`https://api-fulfill.dataexchange.us-east-1.amazonaws.com/v1`)
+async function FetchAPI(){ 
+    const response = await fetch("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1", {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Njk4MWVhY2Q1MzNmZGE1MTQ0MjJmYjhiNTg2YzExYyIsInN1YiI6IjY2NDU0MDAzY2M2ZWUyNmVmZDVjYTEwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ok2hDpRa1efANtr1GqM83v6uBTPL_60oFizxrTHxYHI"
+        },})
     .then((res) => res.json())
     .then((data) =>{
         console.log(data)
