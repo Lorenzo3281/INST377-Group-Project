297cfa6fd25b03b0b004d3e725d3a68b9aeef44e
ceeja <conradj26@gmail.com>
2024-05-19 02:29:00 -0400

    added spacing to lists and making chart

--- INST377 Group Project Homepage Javascript.js
+++ INST377 Group Project Homepage Javascript.js
@@ -111,6 +111,28 @@ function displayTrendingPeople(people) {
         }
     });
 }
+function fetchtoprated() {
+    fetch("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1", {
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Njk4MWVhY2Q1MzNmZGE1MTQ0MjJmYjhiNTg2YzExYyIsInN1YiI6IjY2NDU0MDAzY2M2ZWUyNmVmZDVjYTEwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ok2hDpRa1efANtr1GqM83v6uBTPL_60oFizxrTHxYHI"
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("Error getting top rated data: " + response.statusText);
+        }
+    })
+    .then(data => {
+        displayTopratedChart(data.results);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+}
+
 
 document.addEventListener("DOMContentLoaded", () => {
     fetchMoviesNowPlaying();
